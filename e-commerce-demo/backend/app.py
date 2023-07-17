1f22f88da8c2ea04f53a0e3dbae5dccf25f4e370
OwenPendrighElliott <owenpendrighelliott@gmail.com>
2023-07-17 10:55:45 +1000

    ui fix and docker fix

--- e-commerce-demo/backend/app.py
+++ e-commerce-demo/backend/app.py
@@ -1,5 +1,6 @@
+import logging
 from flask import Flask, jsonify, request
-from .marqo_search import search
+from marqo_search import search
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -15,15 +16,26 @@ def health():
 @app.route("/api/search_marqo", methods=["POST"])
 def search_marqo():
     data = request.get_json()
+    app.logger.debug(f"Data recieved: {data}")
     query = data.get("query")
     more_of = data.get("moreOf")
     less_of = data.get("lessOf")
     limit = data.get("limit")
-    results = search(query, more_of, less_of, limit)
-
+    try:
+        results = search(query, more_of, less_of, limit)
+        app.logger.debug(f"Found {len(results)} results")
+    except Exception as e:
+        app.logger.debug(str(e))
+    
     response = {"results": [dict(r) for r in results]}
     return response, 200
 
 
 if __name__ == "__main__":
     app.run()
+
+
+if __name__ != '__main__':
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
